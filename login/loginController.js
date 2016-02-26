9eca517615086600e3ba653e22afd5297dfd9b3c
kvivek <kvivek@INBlr-kvivek.local>
2016-02-27 01:37:21 +0530

    controller is working

--- login/loginController.js
+++ login/loginController.js
@@ -1,16 +1,19 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('GreetingController',[
-  '$scope' , function($scope) {
+myApp.controller('GreetingController', ['$scope', function($scope) {
+    controller = this;
 
-      controller = this;
-  	//$scope.password = "Initial Value";
-
-      controller.login.password = $scope.login.password;
-      controller.submit = function() {
-        console.log(controller.login.password);
-  }
-  $scope.help = function() {
-		console.log($scope.password);
-  }
+    $scope.helpEnabled = false;
+      $scope.submit = function() {
+          controller.password = $scope.password;
+          controller.loginId = $scope.loginId;
+         // loginFacade.login(controller.loginId, controller.password);
+        console.log($scope.password);
+        console.log($scope.loginId);
+      }
+      $scope.help = function() {
+		$scope.helpEnabled = true;
+      }
+      $scope.brief = "Why do we need to login and register";
+      return controller.init;
 }]);
---
+++ login/loginFacade.js
@@ -0,0 +1,10 @@
+define(, function(){
+    
+    login function(loginId, password) = {
+    console.log($scope.password);
+    console.log($scope.loginId);
+}
+    return {
+        login: login
+    }
+});
