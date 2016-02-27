6ab753ae3f01199e3506e16ed92429ba3e515b57
kvivek <kvivek@INBlr-kvivek.local>
2016-02-27 13:03:52 +0530

    login is working but not on chrome

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2.bower install
 3.grunt
 
+Important: May not be working on chrome because of local json
--- login/loginController.js
+++ login/loginController.js
@@ -1,25 +1,55 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('GreetingController', ['$scope', function ($scope) {
+myApp.controller('GreetingController', ['$scope', '$http', '$q', function ($scope, $http, $q) {
     "use strict";
     var controller = this;
 
+    $scope.isSucess = false;
     $scope.resiter = false;
     $scope.helpEnabled = false;
+    controller.credentials = null;
     $scope.submit = function () {
         $scope.resiter = false;
         $scope.helpEnabled = false;
         controller.password = $scope.password;
         controller.loginId = $scope.loginId;
-         // loginFacade.login(controller.loginId, controller.password);
         console.log($scope.password);
         console.log($scope.loginId);
+        $q.when($http.get('login_promise/credential.json'))
+            .then(function (response) {
+                controller.credentials = response.data;
+            });
+        if (controller.credentials) {
+            if (controller.credentials.loginPassword) {
+                if (controller.credentials.loginPassword[0].loginId === controller.loginId && controller.credentials.loginPassword[0].password === controller.password) {
+                    $scope.isSucess = true;
+                    $scope.sucess = "you are sucessfully loged in";
+                } else {
+                    $scope.isSucess = true;
+                    $scope.sucess = "your login has failed";
+                }
+            } else {
+                $scope.isSucess = true;
+                $scope.sucess = "your login has failed";
+            }
+        } else {
+            $scope.isSucess = true;
+            $scope.sucess = "your login has failed";
+        }
+//        return $q.when("login_promise/credential.json")
+//            .then(function (result) {
+//            return result;
+//        
+//        });
+         // loginFacade.login(controller.loginId, controller.password);
     };
     $scope.help = function () {
+        $scope.isSucess = false;
         $scope.helpEnabled = true;
         $scope.resiter = false;
     };
     $scope.register = function () {
+        $scope.isSucess = false;
         $scope.resiter = true;
         $scope.helpEnabled = false;
         $scope.hasToGoto = "goTo(\"../userDetail/User_details_form.html\")";
