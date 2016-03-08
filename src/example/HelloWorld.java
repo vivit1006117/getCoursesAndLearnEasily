40ce6c2c0dd20709eccf10135d4f53510b80235f
vivek kumar <vivit1006117@gmail.com>
2016-03-08 23:52:40 +0530

    making folder structure

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,22 +1,22 @@
 package example;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 
-/**
- * Created by kvivek on 3/8/16.
- */
 @WebService()
 public class HelloWorld {
-  @WebMethod
-  public String sayHelloWorldFrom(String from) {
-    String result = "Hello, world, from " + from;
-    System.out.println(result);
-    return result;
-  }
-  public static void main(String[] argv) {
-    Object implementor = new HelloWorld ();
-    String address = "http://localhost:9000/HelloWorld";
-    Endpoint.publish(address, implementor);
-  }
+    @WebMethod
+    public String sayHelloWorldFrom(String from) {
+        String result = "Hello, world, from " + from;
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] argv) {
+        HelloWorld implementor = new HelloWorld();
+        String hello = implementor.sayHelloWorldFrom("Vivek kumar");
+        String address = "http://localhost:9000/HelloWorld";
+        //Endpoint.publish(address, implementor);
+    }
 }
